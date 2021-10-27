1c85cfcba95c67691ff87bfc50d74e64443442f4
jessy <jessy.hanzo@airbus.com>
2021-10-27 10:33:08 +0200

    separate files

--- src/main/kotlin/com/jho/centrale/CentraleApplication.kt
+++ src/main/kotlin/com/jho/centrale/CentraleApplication.kt
@@ -1,26 +1,17 @@
 package com.jho.centrale
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
 
 @SpringBootApplication(exclude = [ DataSourceAutoConfiguration::class ])
+@ComponentScan
+
 class CentraleApplication
 
 fun main(args: Array<String>) {
 	runApplication<CentraleApplication>(*args)
 }
-
-@RestController
-class MessageResource {
-	@GetMapping
-	fun index(): List<Message> = listOf(
-		Message("1", "Hello!"),
-		Message("2", "Bonjour!"),
-		Message("3", "Privet!"),
-	)
-}
-
-data class Message(val id: String?, val text: String)
